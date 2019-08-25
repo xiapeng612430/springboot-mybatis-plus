4cbd869442efc4ec5fedf5945131dc4d4d1f3ded
夏先鹏 <951256916>
2019-08-26 01:31:50 +0800

    deleteById()

---
+++ src/test/java/com/ims/mp/DeleteTest.java
@@ -0,0 +1,26 @@
+package com.ims.mp;
+
+import com.ims.mp.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-08-26 01:29
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeleteTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    public void deleteById() {
+        int rows = userMapper.deleteById(1165326669666398210L);
+        System.out.println("delete rows : " + rows);
+    }
+}
