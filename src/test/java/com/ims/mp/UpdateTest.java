cf20389895bd5377c6f146967eeab5d9b2adc6bd
夏先鹏 <951256916>
2019-08-26 01:11:04 +0800

    updateById(Entity)

---
+++ src/test/java/com/ims/mp/UpdateTest.java
@@ -0,0 +1,31 @@
+package com.ims.mp;
+
+import com.ims.mp.entity.User;
+import com.ims.mp.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-08-26 01:04
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UpdateTest {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Test
+    public void updateByIds() {
+        User user = new User();
+        user.setId(1088248166370832385L);
+        user.setAge(26);
+        user.setEmail("wtf@qq.com");
+        int rows = userMapper.updateById(user);
+        System.out.println("update : " + i);
+    }
+}
